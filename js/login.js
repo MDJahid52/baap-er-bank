ed04830f75cb8cd58e2af196078c4ba8802b2a24
MDJahid52 <jhjahidhasan0052@gmail.com>
2023-08-19 11:07:08 +0600

    baaper bank login page

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+    const userEmail = document.getElementById('user-email');
+    const email = userEmail.value;
+    
+    const userPassword = document.getElementById('user-password');
+    const password = userPassword.value;
+
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        alert('Password is incorrect')
+    }
+})
